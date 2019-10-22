35e4a32743aa3c85c24d95a702222b3fd38d32b1
Matt Moyer <tfranks247@gmail.com>
2019-10-21 21:36:14 -0400

    trying to make math2 to run, it runs w/o errors but wont load on server port

---
+++ src/main/java/com/example/demo/Variables.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+public class Variables {
+        public static void main(String[] args) {
+
+            int myNumber = 2;
+            int myOtherNumber = (int) 1.7938;
+
+            System.out.println("myNumber");
+            System.out.println("myOtherNumber");
+        }
+    }
+
