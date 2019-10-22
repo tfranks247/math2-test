50548dcc20bac7b601cfbd5ac3a9bbc683026f7e
Matt Moyer <tfranks247@gmail.com>
2019-10-21 21:53:31 -0400

    last push

--- src/main/java/com/example/demo/Variables.java
+++ src/main/java/com/example/demo/Variables.java
@@ -3,11 +3,14 @@ package com.example.demo;
 public class Variables {
         public static void main(String[] args) {
 
-            int myNumber = 2;
-            int myOtherNumber = (int) 1.7938;
+             int myNumber;
+             double myOtherNumber;
 
-            System.out.println("myNumber");
-            System.out.println("myOtherNumber");
+             myNumber = 2;
+             myOtherNumber = 1.7938;
+
+            System.out.println(myNumber);
+            System.out.println(myOtherNumber);
         }
     }
 
